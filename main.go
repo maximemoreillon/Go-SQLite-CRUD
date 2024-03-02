c6f2bbeed7c91446db4187d6a8943114c4616d38
Maxime Moreillon <m.moreillon@gmail.com>
2024-03-02 13:57:16 +0900

    file splitting

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"strconv"
 
@@ -13,30 +12,6 @@ type Movie struct {
 	ID int
 	Title string
 	Year int
-}
-
-func openDb () *sql.DB {
-	const file string = "movies.db"
-	db, err := sql.Open("sqlite3", file)
-	
-	if err != nil {
-		panic(err)
-	}
-	return db
-}
-
-func createTableIfNotExists (db *sql.DB) {
-	const create string = `
-		CREATE TABLE IF NOT EXISTS "movies" (
-		"id"	INTEGER,
-		"title"	TEXT NOT NULL,
-		"year"	INTEGER,
-		PRIMARY KEY("id" AUTOINCREMENT)
-	);`
-	_, err := db.Exec(create);
-	if err != nil {
-		panic(err)
-	}
 }
 
 
