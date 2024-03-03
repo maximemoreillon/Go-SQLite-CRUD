f773df16cb0e8399d7b3fc7c32ac3d8424f845a6
Maxime Moreillon <m.moreillon@gmail.com>
2024-03-03 16:17:47 +0900

    minor

--- main.go
+++ main.go
@@ -25,39 +25,43 @@ func main () {
 	// Printing list of movies before insertion
 	var movies []Movie
 	movies = readMovies(db)
+	fmt.Println("Current list of movies:")
 	for _, movie := range movies {
-		fmt.Printf("[%s] %s (%s)\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
+		fmt.Printf("- [%s] %s (%s)\n\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
 	}
+	fmt.Println("")
 
+	// Creating a movie
 	newMovie := createMovie(db)
 	movieId :=  newMovie.ID
+	fmt.Printf("Created [%s] %s (%s)\n\n", strconv.Itoa(newMovie.ID), newMovie.Title, strconv.Itoa(newMovie.Year))
 
 	// Printing list of movies after creation
 	movies = readMovies(db)
+	fmt.Println("Current list of movies:")
 	for _, movie := range movies {
-		fmt.Printf("[%s] %s (%s)\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
+		fmt.Printf("- [%s] %s (%s)\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
 	}
+	fmt.Println("")
 
 	// Printing a single movie
-	var movie Movie
-	movie = readMovie(db, movieId)
-	fmt.Printf("[%s] %s (%s)\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
+	movie := readMovie(db, movieId)
+	fmt.Printf("Queried movie: [%s] %s (%s)\n\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
 
+	// Updating a movie
+	updatedMovie := updateMovie(db, movieId, "Interstellar")
+	fmt.Printf("Updated movie [%s] %s (%s)\n\n", strconv.Itoa(updatedMovie.ID), updatedMovie.Title, strconv.Itoa(updatedMovie.Year))
 
-	updateMovie(db, movieId, "Interstellar")
-
-	// Checking update
-	movie = readMovie(db, movieId)
-	fmt.Printf("[%s] %s (%s)\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
-
-
+	// Deleting a movie
 	deleteMovie(db, movieId)
 
 	// Checking state after deletion
 	movies = readMovies(db)
+	fmt.Println("Current list of movies:")
 	for _, movie := range movies {
-		fmt.Printf("[%s] %s (%s)\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
+		fmt.Printf("- [%s] %s (%s)\n", strconv.Itoa(movie.ID), movie.Title, strconv.Itoa(movie.Year))
 	}
+	fmt.Println("")
 
 	db.Close()
 
